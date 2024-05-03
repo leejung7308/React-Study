446856cd482a55049878eef33d0e04ed9bc4d23e
leejung7308 <leekkzzzz@naver.com>
2024-05-03 23:49:51 +0900

    router 다루기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import Hello from './Components/Hello';
-import Welcome from './Components/Welcome';
-import styles from './Components/App.module.css';
+import Header from './Components/Header';
+import DayList from './Components/DayList';
+import Day from './Components/Day';
+import EmptyPage from './Components/EmptyPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='APP'>
-    <Hello/>
-    <Hello/>
-    <Hello/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element = {<DayList />} />
+          <Route path="/day/:day" element = {<Day />} />
+          <Route path="*" element = {<EmptyPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,19 @@
-import React from "react";
+import { Link } from "react-router-dom"
 
-function Header(props){
+export default function Header(){
     return(
-        <div>
-            <header>
-                <h1>헤더입니다.</h1>
-            </header>
+        <div className="header">
+            <h1>
+                <Link to="/">토익 영단어(고급)</Link>
+            </h1>
+            <div className="menu">
+                <a href="#x" className="link">
+                    단어 추가
+                </a>
+                <a href="#x" className="link">
+                    Day 추가
+                </a>
+            </div>
         </div>
     );
 }
-
-export default Header;
