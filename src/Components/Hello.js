2d3916637487df706429bad3e1ae08f0478820fc
leejung7308 <leekkzzzz@naver.com>
2024-05-03 22:02:12 +0900

    props 다루기

--- src/Components/Hello.js
+++ src/Components/Hello.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
+import UserName from "./UserName";
 
-export default function Hello(){
+export default function Hello({age}){
     const [name, setName] = useState("이정");
-    function changeName(){
-        setName(name === "이정" ? "정이" : "이정")
-    }
+    const msg = age > 19 ? "성인" : "미성년자";
     return(
-        <div>
-            <h1>State</h1>
-            <h2 id="name">{name}</h2>
-            <button onClick={changeName}>Change</button>
+        <div className="User">
+            <h2 id="name">
+                {name}({age}) : {msg}
+            </h2>
+            <UserName name={name}/>
+            <button onClick={()=>{
+                setName(name === "이정" ? "정이" : "이정");
+            }}
+            >
+                Change
+            </button>
         </div>
     );
 }
