52b2547016666c6e9f3566ffbd1b854ab5d883eb
leejung7308 <leekkzzzz@naver.com>
2024-05-04 00:34:10 +0900

    json-server 다루기

--- src/Components/Day.js
+++ src/Components/Day.js
@@ -1,19 +1,27 @@
-import dummy from '../db/data.json';
+import Word from './Word';
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function Day(){
     const day = useParams().day;
-    const wordList = dummy.words.filter(word=>(word.day === Number(day)))
+    const [words, setWords] = useState([]);
+
+    useEffect(()=>{
+        fetch(`http://localhost:3001/words?day=${day}`)
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setWords(data);
+        });
+    }, [day]);
     return (
         <>
             <h2>Day {day}</h2>
             <table>
                 <tbody>
-                {wordList.map(word=>(
-                    <tr key={word.id}>
-                        <td>{word.eng}</td>
-                        <td>{word.kor}</td>
-                    </tr>
+                {words.map(word=>(
+                    <Word word = {word} key = {word.id}/>
                 ))}
                 </tbody>
             </table>
--- src/Components/DayList.js
+++ src/Components/DayList.js
@@ -1,14 +1,29 @@
 import { Link } from 'react-router-dom';
 import dummy from '../db/data.json';
+import { useEffect, useState } from 'react';
 
 export default function DayList() {
-    console.log(dummy);
-    return <ul className="list_day">
-        {dummy.days.map(day => (
-            <li key={day.id}>
-                <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-            </li>
-        ))}
-        <li></li>
-    </ul>
+    const [days, setDays] = useState([]);
+    
+    useEffect(()=>{
+        fetch('http://localhost:3001/days')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setDays(data);
+        });
+    }, []);
+
+    return( 
+        <>
+            <ul className="list_day">
+                {dummy.days.map(day => (
+                    <li key={day.id}>
+                        <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
