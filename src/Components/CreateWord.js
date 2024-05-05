fdccd5aa0811813c1ba53a770d649275ca40fc68
leejung7308 <leekkzzzz@naver.com>
2024-05-05 22:43:21 +0900

    이전, 다음 페이지 구현

--- src/Components/CreateWord.js
+++ src/Components/CreateWord.js
@@ -1,32 +1,39 @@
 import useFetch from "../Hooks/useFetch";
 import { useRef } from "react";
 import { useNavigate } from "react-router";
+import { useState } from "react";
 
 export default function CreateWord(){
     const days = useFetch('http://localhost:3001/days');
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
 
     function onSubmit(e){
         e.preventDefault();
         
-        fetch(`http://localhost:3001/words/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                day: dayRef.current.value,
-                eng: engRef.current.value,
-                kor: korRef.current.value,
-                isDone: false,
-            }),
-        })
-        .then(res => {
-            if(res.ok){
-                alert('생성이 완료되었습니다.');
-                navigate(`/day/${dayRef.current.value}`);
-            }
-        });
+        if(!isLoading){
+            setIsLoading(true);
+        
+            fetch(`http://localhost:3001/words/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    day: dayRef.current.value,
+                    eng: engRef.current.value,
+                    kor: korRef.current.value,
+                    isDone: false,
+                }),
+            })
+            .then(res => {
+                if(res.ok){
+                    alert('생성이 완료되었습니다.');
+                    navigate(`/day/${dayRef.current.value}`);
+                    setIsLoading(false);
+                }
+            });
+        }
     }
 
     const engRef = useRef(null);
@@ -52,7 +59,13 @@ export default function CreateWord(){
                     ))}
                 </select>
             </div>
-            <button>저장</button>
+            <button
+            style={{
+                    opacity: isLoading ? 0.3 : 1,
+                }}
+            >
+                {isLoading ? "Saving..." : "저장"}
+            </button>
         </form>
     )
 }
--- src/Components/Day.js
+++ src/Components/Day.js
@@ -2,14 +2,39 @@ import Word from './Word';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import useFetch from '../Hooks/useFetch';
+import { Link } from 'react-router-dom';
 
 export default function Day(){
     const day = useParams().day;
     const words = useFetch(`http://localhost:3001/words?day=${day}`);
+    const days = useFetch('http://localhost:3001/days');
 
     return (
         <>
-            <h2>Day {day}</h2>
+            <div className="day_header">
+                <h2>Day {day}</h2>
+                <div className="day_menu">
+                    <Link 
+                        className='link' 
+                        to={`/day/${Number(day) === 1 ? day : Number(day) - 1}`}
+                        style={
+                            Number(day) === 1 ? {opacity: 0.3} : {opacity: 1}
+                        }
+                    >
+                        Prev
+                    </Link>
+                    <Link 
+                        className='link' 
+                        to={`/day/${Number(day) === days.length ? day : Number(day) + 1}`}
+                        style={
+                            Number(day) === days.length ? {opacity: 0.3} : {opacity: 1}
+                        }
+                    >
+                        Next
+                    </Link>
+                </div>
+            </div>
+            {words.length === 0 && <span>Loading...</span>}
             <table>
                 <tbody>
                 {words.map(word=>(
